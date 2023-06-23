1bad4bf03636a4b27fe36739be8127aa21091106
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2023-06-23 02:26:47 -0300

    add graphs

--- src/components/Graphics/Graph.tsx
+++ src/components/Graphics/Graph.tsx
@@ -1,6 +1,22 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import ReactApexChart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
+
+const options: ApexOptions = {
+  chart: {
+    zoom: {
+      enabled: false
+    }
+  },
+};
+
+const series = [{
+
+  name: 'Teste',
+  data: [31, 40, 28, 51, 42, 109, 100]
+}, ]
 
 const defaultTheme = createTheme();
 
@@ -11,7 +27,7 @@ export default function Graph() {
       <ThemeProvider theme={defaultTheme}>
         <Container> 
         <CssBaseline />
-
+        <ReactApexChart options={options} type='bar' height={500} width={700} series={series}/>
         </Container>
       </ThemeProvider>
     </div>
